8c48ec2bf36c3a891d5574d1c89099233524224a
softacoder <113185323+softacoder@users.noreply.github.com>
2024-01-03 18:03:31 +0000

    Commit update

--- popup/background.js
+++ popup/background.js
@@ -1,5 +1,24 @@
 chrome.runtime.onInstalled.addListener((details) => {
-  console.log(details);
+  chrome.contextMenus.create({
+    title: "Test Context Menu",
+    id: "contextMenu1",
+    contexts: ["page, selection"],
+  });
+  chrome.contextMenus.onClicked.addListener((event) => {
+    console.log(event);
+  });
+  chrome.contextMenus.create({
+    title: "Test Context Menu 1",
+    id: "contextMenu2",
+    parentId: "contextMenu1",
+    contexts: ["page, selection"],
+  });
+  chrome.contextMenus.create({
+    title: "Test Context Menu 2",
+    id: "contextMenu3",
+    parentId: "contextMenu1",
+    contexts: ["page, selection"],
+  });
 });
 
 console.log("background script running");
