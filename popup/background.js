24e93c67069fa901f3dd2a91d573e24e77ce996a
softacoder <113185323+softacoder@users.noreply.github.com>
2024-01-04 15:47:45 +0000

    commit added ContentScript

--- popup/background.js
+++ popup/background.js
@@ -6,7 +6,7 @@ chrome.runtime.onInstalled.addListener((details) => {
   });
   chrome.contextMenus.onClicked.addListener((event) => {
     chrome.tabs.create({
-      url: `https://www.imbd.com/find?q=${event.selectionText}&ref_=nvsr_sm`,
+      url: `https://www.imbd.com/find?q=${event.selectionText}&ref_=nv_sr_sm`,
     });
   });
 });
