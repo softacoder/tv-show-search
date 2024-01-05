1fd8359c477f9b9c6e07a5f5e71c5bafecc70842
softacoder <113185323+softacoder@users.noreply.github.com>
2024-01-05 16:28:46 +0000

    commit final

--- popup/background.js
+++ popup/background.js
@@ -1,19 +1,31 @@
 chrome.runtime.onInstalled.addListener((details) => {
+  chrome.storage.local.set({
+    shows: [],
+  });
   chrome.contextMenus.create({
     title: "Test Context Menu",
     id: "contextMenu1",
     contexts: ["page, selection"],
   });
-  chrome.contextMenus.onClicked.addListener((event) => {
-    chrome.tabs.create({
-      url: `https://www.imbd.com/find?q=${event.selectionText}&ref_=nv_sr_sm`,
-    });
+  chrome.contextMenus.create({
+    title: "Read this Text",
+    id: "contextMenu2",
+    contexts: ["page, selection"],
   });
-});
-
-chrome.runtime.onMesssage.addListener((msg, sender, sendResponse) => {
-  console.log(msg);
-  console.log(sender);
-  sendResponse("received message from background");
-  chrome.tabs.sendMessage(sender.tab.id, "Got your message from background!");
+  chrome.contextMenus.onClicked.addListener((event) => {
+    if (event.menuItenId === "contextMenu1") {
+      fetch(`https://api.tvmaze.com/search/shows?q=${event.selectionText}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          chrome.storage.local.set({
+            shows: data,
+          });
+        });
+    } else if (event.menuItemId === "contextMenu2") {
+      chrome.tts.speak(event.selectionText, {
+        rate: 2,
+      });
+    }
+  });
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,19 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log(message);
-  console.log(sender);
+chrome.storage.local.get(["shows"], (res) => {
+  for (const show of res.shows) {
+    renderShow(show);
+  }
 });
+
+function renderShow(show) {
+  const showDiv = document.createElement("div");
+
+  const title = document.createElement("h3");
+  title.textContent = show.show.name;
+
+  const image = document.createElement("img");
+  image.src = show.show.image ? show.show.medium : null;
+
+  showDiv.appendChild(title);
+  showDiv.appendChild(image);
+  document.body.appendChild(showDiv);
+}
