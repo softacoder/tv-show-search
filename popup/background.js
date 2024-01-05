52f3395e0da9ddb6834c6f715d053e897b6df39a
softacoder <113185323+softacoder@users.noreply.github.com>
2024-01-05 10:31:25 +0000

    commit created popup.js and update maps

--- popup/background.js
+++ popup/background.js
@@ -11,4 +11,9 @@ chrome.runtime.onInstalled.addListener((details) => {
   });
 });
 
-console.log("background script running");
+chrome.runtime.onMesssage.addListener((msg, sender, sendResponse) => {
+  console.log(msg);
+  console.log(sender);
+  sendResponse("received message from background");
+  chrome.tabs.sendMessage(sender.tab.id, "Got your message from background!");
+});
